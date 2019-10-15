b3960b9c21a1fe063bbc2bf8654997feacb4025e
Nitish Aggarwal <royalnitish21@gmail.com>
2019-10-15 22:47:39 +0530

    feat: Initial commit, Added marshmallow integration

---
+++ schemas/item.py
@@ -0,0 +1,12 @@
+from ma import ma
+from models.item import ItemModel
+from models.store import StoreModel
+
+
+class ItemSchema(ma.ModelSchema):
+    class Meta:
+        model = ItemModel
+        load_only = ('store',)
+        dump_only = ('id',)
+        include_fk = True
+
